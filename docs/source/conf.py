3b48efa95ad672023cd063568fb29c04805f6859
Amit Arora <aroraai@amazon.com>
2025-03-23 16:41:46 -0400

    fixing readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
+
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
